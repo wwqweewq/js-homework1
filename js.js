9f44dc4c29d1b69b8f2fa07694437ec5ebcbd924
wwqweewq <koleso228999@gmail.com>
2022-09-14 19:05:30 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+// let value = prompt('Please, enter a year')
+// console.log(value);
+// console.log(2022 - value);
+const a = prompt('Enter a number, please');
+let b = 1;
+console.log(a);
+console.log(a * b + 1);
+console.log(a * b+ 2);
